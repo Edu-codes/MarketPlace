33039250d97e531392d3c4ce62b0a77561f05e75
Edu-codes <daniel.gamb98@hotmail.com>
2025-09-11 22:33:21 -0500

    Campos nuevos en formulario de productos -Administrador

--- React-MP/src/components/Formularios/FormUpdateProduct.jsx
+++ React-MP/src/components/Formularios/FormUpdateProduct.jsx
@@ -10,6 +10,7 @@ import {
   InputLabel,
   FormControl,
 } from '@mui/material';
+import BtnFotos from "../Button/Admin/BtnFoto";
 
 const styleModal = {
   position: 'absolute',
@@ -24,19 +25,31 @@ const styleModal = {
 };
 
 
-const FormularioProductoModal = ({ handleEliminar, formData, categorias, handleChange, handleGuardar, handleCerrarModal, subCategorias, unidadMedida }) => {
+const FormularioProductoModal = ({ handleEliminar, handleNuevaImagen, formData, estado, handleChange, handleGuardar, handleCerrarModal, cargarFotos, subCategorias, unidadMedida, fotoProductos }) => {
   return (
     <Modal open={true} onClose={handleCerrarModal}>
       <Box sx={styleModal}>
         <Typography variant="h6" component="h2" gutterBottom>
           Editar Producto
         </Typography>
+        <Button
+          variant="contained"
+          component="label"
+          color="primary"
+        >
+          <input
+            type="file"
+            hidden
+            onChange={handleNuevaImagen}
 
+          />
+          Subir Foto
+        </Button>
         <TextField
           fullWidth
           label="Nombre"
-          name="nombrePro"
-          value={formData.nombrePro}
+          name="NombrePro"
+          value={formData.NombrePro}
           onChange={handleChange}
           margin="normal"
         />
@@ -45,8 +58,8 @@ const FormularioProductoModal = ({ handleEliminar, formData, categorias, handleC
           fullWidth
           label="Precio"
           type="number"
-          name="precio"
-          value={formData.precio}
+          name="Precio"
+          value={formData.Precio}
           onChange={handleChange}
           margin="normal"
         />
@@ -55,40 +68,23 @@ const FormularioProductoModal = ({ handleEliminar, formData, categorias, handleC
           fullWidth
           label="Cantidad"
           type="number"
-          name="cantidad"
-          value={formData.cantidad}
+          name="Stock"
+          value={formData.Stock}
           onChange={handleChange}
           margin="normal"
         />
 
         <FormControl fullWidth margin="normal">
-          <InputLabel>Categoría</InputLabel>
-          <Select
-            name="idCat"
-            value={formData.idCat || ''}
-            onChange={handleChange}
-            label="Categoría"
-          >
-            <MenuItem value="">Seleccione una opción</MenuItem>
-            {categorias.map((cat) => (
-              <MenuItem key={cat.id} value={cat.id.toString()}>
-                {cat.nombreCat}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl fullWidth margin="normal">
           <InputLabel>Subcategoría</InputLabel>
           <Select
-            name="subcatId"
-            value={formData.subcatId || ''}
+            name="SubCategoriaId"
+            value={formData.SubCategoriaId || ''}
             onChange={handleChange}
-            label="Subcategoría"
+            label="SubCategoría"
           >
             <MenuItem value="">Seleccione una opción</MenuItem>
             {subCategorias.map((subCat) => (
-              <MenuItem key={subCat.id} value={subCat.id.toString()}>
+              <MenuItem key={subCat.Id} value={subCat.id.toString()}>
                 {subCat.nombreSubCat}
               </MenuItem>
             ))}
@@ -96,23 +92,56 @@ const FormularioProductoModal = ({ handleEliminar, formData, categorias, handleC
         </FormControl>
 
         <FormControl fullWidth margin="normal">
+          <InputLabel>Estado</InputLabel>
+          <Select
+            name="EstadoId"
+            value={formData.EstadoId || ''}
+            onChange={handleChange}
+            label="estado"
+          >
+            <MenuItem value="">Seleccione una opción</MenuItem>
+            {
+              estado.map((est) => (
+                <MenuItem key={est.Id} value={est.id.toString()}>
+                  {est.nombre}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
           <InputLabel>Unidad de Medida</InputLabel>
           <Select
-            name="unidadId"
-            value={formData.unidadId || ''}
+            name="UniMedId"
+            value={formData.UniMedId || ''}
             onChange={handleChange}
             label="Unidad de Medida"
           >
             <MenuItem value="">Seleccione una opción</MenuItem>
             {unidadMedida.map((u) => (
               <MenuItem key={u.id} value={u.id.toString()}>
-                {u.nombreUnidadMedida}
+                {u.nombreUniMed}
               </MenuItem>
             ))}
           </Select>
         </FormControl>
 
+        <TextField
+          fullWidth
+          label="Descripcion"
+          type="text"
+          name="Descripcion"
+          value={formData.Descripcion}
+          onChange={handleChange}
+          margin="normal"
+        />
+
         <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3 }}>
+
+          <BtnFotos
+            fotosPorId={fotoProductos} // ✅ este sí es el array de fotos
+            recargarFotos={cargarFotos}// si necesitas recargar después de eliminar
+          />
 
           <Button variant="contained" color="primary" onClick={handleGuardar} sx={{ mr: 1 }}>
             Guardar
@@ -121,7 +150,7 @@ const FormularioProductoModal = ({ handleEliminar, formData, categorias, handleC
           <Button variant="outlined" color="secondary" onClick={handleEliminar} sx={{ mr: 1 }}>
             Eliminar
           </Button>
-          
+
           <Button variant="outlined" color="secondary" onClick={handleCerrarModal}>
             Cancelar
           </Button>
