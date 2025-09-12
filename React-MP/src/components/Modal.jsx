5129143f6e4891e522a731c8eb50f4a190fa0d9c
Edu-codes <daniel.gamb98@hotmail.com>
2025-09-11 22:29:22 -0500

    Estilos para Modal

--- React-MP/src/components/Modal.jsx
+++ React-MP/src/components/Modal.jsx
@@ -4,17 +4,17 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  maxWidth: '100%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
 };
 
 export default function ModalUsuario({ isOpen, onClose, title, children }) {
@@ -24,6 +24,7 @@ export default function ModalUsuario({ isOpen, onClose, title, children }) {
       onClose={onClose}
       aria-labelledby="parent-modal-title"
       aria-describedby="parent-modal-description"
+      sx={{ minWidth: '100%' }}
     >
       <Box sx={{ ...style }}>
         <Button onClick={onClose} style={{ float: 'right' }}>X</Button>
