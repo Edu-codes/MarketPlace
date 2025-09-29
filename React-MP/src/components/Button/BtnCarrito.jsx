dbc6cdeafa042d4367ab2569321c6d9a7a0771e7
Edu-codes <daniel.gamb98@hotmail.com>
2025-09-28 23:23:07 -0500

    Muestra total de compra. Boton para ir al  portal de pagos

--- React-MP/src/components/Button/BtnCarrito.jsx
+++ React-MP/src/components/Button/BtnCarrito.jsx
@@ -1,10 +1,33 @@
 import { useEffect, useState } from "react"
+
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+//para la cantidad del carrito
 import Badge from '@mui/material/Badge';
+
+//importamos modal
 import ModalUsuario from "../Modal";
-import { Button } from "@mui/material";
+import Button from '@mui/material/Button';
+
+//navegar
+import { useNavigate } from "react-router";
 
 export default function BtnCarrito() {
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
+    const handleAbrirModal = () => {
+        handleCarrito()
+        setOpenModal(true)
+    }
+
 
     const [cantidadBadge, setCantidadBadge] = useState(0);
     const [openModal, setOpenModal] = useState(false);
@@ -33,10 +56,6 @@ export default function BtnCarrito() {
         }
     }, [])
 
-    const handleAbrirModal = () => {
-        handleCarrito()
-        setOpenModal(true)
-    }
 
     const handleCarrito = () => {
 
@@ -47,18 +66,28 @@ export default function BtnCarrito() {
 
         setProductosCarrito(carrito)
 
-        console.log(productosCarrito)
+        console.log("prdocutos carrito", productosCarrito)
     }
 
     const handleEliminarDelCarrito = (id) => {
         const user = JSON.parse(sessionStorage.getItem('user'))
         const key = `cart_${user.doc}`
-        const nuevoCarrito = productosCarrito.filter((producto) => producto.id !== id   )
+        const nuevoCarrito = productosCarrito.filter((producto) => producto.referencia !== id)
         localStorage.setItem(key, JSON.stringify(nuevoCarrito))
         setProductosCarrito(nuevoCarrito)
     }
 
+    //Operaciones del modal
+    const total = productosCarrito.reduce(
+        (acc, producto) => acc + producto.precio * producto.cantidad,
+        0
+    );
 
+    const totalFormateado = new Intl.NumberFormat("es-CO", {
+        style: "currency",
+        currency: "COP",
+        minimumFractionDigits: 0,
+    }).format(total);
 
     return (
         <>
@@ -68,29 +97,104 @@ export default function BtnCarrito() {
                     onClick={() => { handleAbrirModal() }}
 
                 />
-
                 <ModalUsuario
                     isOpen={openModal}
                     onClose={() => setOpenModal(false)}
                     title="Tus Productos"
+
                 >
-                    <ul>
-                        {productosCarrito.map((producto) => (
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexWrap: "wrap",
+                            "& > :not(style)": {
+                                m: 1,
+                                width: "100%",
+                                height: "100%",
+                            },
+                            maxHeight: "400px",
+                            overflowY: "auto",
+                        }}
+                    >
 
-                            
-                            <li key={producto.id}>
-                                
-                                {producto.nombrePro} - ${producto.precio * producto.cantidad} x {producto.cantidad}
-                                <Button
-                                onClick={ () => handleEliminarDelCarrito(producto.id)}
-                                >
-                                    X
-                                </Button>
-                            </li>
+                        {productosCarrito.length === 0 ? (
+                            <Paper
+                                elevation={3}
+                                sx={{
+                                    padding: 2,
+                                    textAlign: "center",
+                                    color: "gray",
+                                }}
+                            >
+                                🛒 Tu carrito está vacío
+                            </Paper>
+                        ) : (
+                            productosCarrito.map((producto) => {
+                                const precioPorCantidad = producto.precio * producto.cantidad;
+                                return (
+                                    <Paper
+                                        key={producto.referencia}
+                                        elevation={3}
+                                        sx={{
+                                            padding: 1,
+                                            display: "flex",
+                                            justifyContent: "space-between",
+                                            alignItems: "center"
+                                        }}>
+                                        <span>
+                                            {producto.nombrePro} - ${precioPorCantidad} x {producto.cantidad}
+                                        </span>
+                                        <Button
+                                            size="small"
+                                            color="error"
+                                            onClick={() => handleEliminarDelCarrito(producto.referencia)}
+                                        >
+                                            X
+                                        </Button>
+                                    </Paper>
+                                );
+                            }
 
-                        ))}
-                    </ul>
+                            )
+                        )
+                        }
+                    </Box>
+
+                    <Box
+                        sx={{
+                            width: "100%",
+                            backgroundColor: "white",
+                            marginTop: "1rem",
+                            marginBottom: "2rem"
+                        }}>
+
+                        TOTAL: {totalFormateado}
+
+                    </Box>
+
+
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            bottom: 0,
+                            left: 0,
+                            width: "100%",
+                            backgroundColor: "white",
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            onClick={() => {
+                                handleNavigation('PortalPagos');
+                                setOpenModal(false)
+                            }}
+                        >
+                            Confirmar compra
+                        </Button>
+                    </Box>
                 </ModalUsuario>
+
             </Badge>
         </>
 
