a222c5fe00248ff444a9619eb751cdad4257e016
Edu-codes <daniel.gamb98@hotmail.com>
2025-09-29 23:23:39 -0500

    se modifica para usar el provider del carrito

--- React-MP/src/main.jsx
+++ React-MP/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { MyProvider } from './context/Context.jsx';
+
+import { CartProvider } from './context/CartContext'
+
 import theme from './theme/theme.js';
 import './index.css'
 import App from './App.jsx'
@@ -11,12 +14,14 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MyProvider>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </CartProvider>
     </MyProvider>
   </StrictMode>,
 )
